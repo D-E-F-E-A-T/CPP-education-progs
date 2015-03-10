aaee8f47fb44deeef940405fceedc6158f5e64fd
Valera Rozuvan <valera.rozuvan@gmail.com>
2015-03-10 18:57:06 +0200

    Renaming and restructuring of folders
    This hierarchy will allow for easier searching of concrete program
    example folder.

--- build_all.bat
+++ build_all.bat
@@ -3,38 +3,38 @@
 @echo. >> build_output.txt
 @echo. >> build_output.txt
 
-@echo 1. simple string concatenation 01 >> build_output.txt
-cd "simple string concatenation 01"
+@echo 001 simple string concatenation >> build_output.txt
+cd "001 simple string concatenation"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
 
-@echo 2. excercise with operators 01 >> build_output.txt
-cd "excercise with operators 01"
+@echo 002 excercise with operators >> build_output.txt
+cd "002 excercise with operators"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
 
-@echo 3. input and type 01 >> build_output.txt
-cd "input and type 01"
+@echo 003 input and type >> build_output.txt
+cd "003 input and type"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
 
-@echo 4. basic cpp types 01 >> build_output.txt
-cd "basic cpp types 01"
+@echo 004 basic cpp types >> build_output.txt
+cd "004 basic cpp types"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
 
-@echo 5. read write first name 01 >> build_output.txt
-cd "read write first name 01"
+@echo 005 read write first name >> build_output.txt
+cd "005 read write first name"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
 
-@echo 6. hello world 01 >> build_output.txt
-cd "hello world 01"
+@echo 006 hello world >> build_output.txt
+cd "006 hello world"
 call build
 type build_output.txt >> ..\build_output.txt
 cd ..
